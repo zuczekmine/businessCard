de25156dca1af41acd9ad1929196ead4c1ff46b5
s-115-20 <s-115-20@gumiforest.ae.krakow.pl>
2018-03-05 16:12:51 +0100

    css added

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -5,23 +5,3 @@ function init() {
 function onDeviceReady() {
 	navigator.notification.beep(1);
 }
-
-function deviceInfo() {
-
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
-
-	navigator.notification.alert(info);
-	
-}
-
-function userInfo() {
-	user = "Anna Żukowska \n 188897"
-	navigator.notification.alert(user);
-}
