8e96b674a56c5e85c4f3899d711f7a6a7393a997
AnnaZ <zuczekmine@gmail.com>
2018-03-11 14:36:57 +0100

    added function in js

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -21,3 +21,20 @@ function internetInfo() {
  
     alert('Connection type: ' + states[networkState]);
 }
+
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
+
+function getMovementInfo() {
+	var options = { frequency: 3000 };  // Update every 3 seconds
+
+	var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+}
