101e94ef9e79d98dc63b7288ef0d5ed85a9d9c7c
AnnaZ <zuczekmine@gmail.com>
2018-03-11 14:04:41 +0100

    added internet info

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -5,3 +5,19 @@ function init() {
 function onDeviceReady() {
 	navigator.notification.beep(1);
 }
+
+function internetInfo() {
+	var networkState = navigator.connection.type;
+ 
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+ 
+    alert('Connection type: ' + states[networkState]);
+}
