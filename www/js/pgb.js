a6c3f670abc99a327db0403934e16eee7f77f64d
AnnaZ <zuczekmine@gmail.com>
2018-03-11 15:10:39 +0100

    change in navigator accelerometer

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -39,7 +39,7 @@ function onError() {
 
 function getMovementInfo() {
 	var options = { frequency: 3000 };  // Update every 3 seconds
-	var watchPos =navigator.accelerometer.getCurrentAcceleration(onSuccess, onError, options);
+	var watchPos =navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
 	if (watchPos == watchPosition) {
 		alert('Nie zmieniles pozycji');
 	} else {
